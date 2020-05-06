5c87b09c19fc6bac6dbb4aec6f1d25a1ce024b53
mirsad <miki.halilcevic@gmail.com>
2020-05-06 13:31:30 +0200

    feat: add read and write repository for all classes
    * Add read repo with multiple faulty sql queires
    * Add mutable repo to update delete and insert with flaws

---
+++ src/repositories/db_wrapper.cr
@@ -0,0 +1,9 @@
+module BrokenCrystal
+   class DBWrapper
+        @queryRunner: DB::Database
+        
+        def initialize()
+            @query_runner = DB.open(ENV["POSTGRES_URL"])
+        end
+    end 
+end
---
+++ src/repositories/mutable_repository.cr
@@ -0,0 +1,34 @@
+module BrokenCrystal
+    class MutableRepository
+        @db: DBWrapper
+
+        def initialize(@table : String)
+        end
+
+        def insert(insert_columns : Array(String), values : Array)
+            @db.query_runner.query("INSERT INTO #{@table}(#{insert_columns.join(", ")}) VALUES(#{values.join(", ")})")
+        end
+
+        def updateAll(update_columns : Array(String), values : String)
+            update_statements = Array(String).new
+            update_columns.each_index { |index| update_statements << "#{columns[index]} = #{values[index]}" }
+
+            @db.query_runner.query("UPDATE #{@table} SET #{update_statements.join(", ")}")
+        end
+
+        def updateById(update_columns : Array(String), values : String, id : String)
+            update_statements = Array(String).new
+            update_columns.each_index { |index| update_statements << "#{columns[index]} = #{values[index]}" }
+
+            @db.query_runner.query("UPDATE #{@table} SET #{update_statements.join(", ")} WHERE id = #{id}")
+        end
+
+        def deleteWhere(condition : String)
+            @db.query_runner.query("DELETE FROM #{@table} WHERE #{condition}") # bad sql baaaad
+        end
+
+        def deleteById(id : String)
+            @db.query_runner.query("DELETE FROM #{@table} WHERE id = #{id}")
+        end
+    end
+end
