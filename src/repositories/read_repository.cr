4faf3eb9eed111f34318deda8b3384eeb4e8146b
mirsad <miki.halilcevic@gmail.com>
2020-05-06 17:19:37 +0200

    fix: mergeeeee

--- src/repositories/read_repository.cr
+++ src/repositories/read_repository.cr
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-module BrokenCrystal
-  class ReadRepository
-    @db : DBWrapper
-
-    def initialize(@table : String)
-    end
-
-    def select
-      @db.query_runner.query("SELECT * FROM #{@table}")
-    end
-
-    def select_where(column : String, value : String | Int32)
-      @db.query_runner.query("SELECT * FROM #{@table} WHERE #{column} = #{value}")
-    end
-
-    def select_where_in(column : String, value : Array(String | Int32))
-      @db.query_runner.query("SELECT * FROM #{@table} WHERE #{column} IN #{value}")
-    end
-
-    def select_where_like(column : String, value : String)
-      @db.query_runner.query("SELECT * FROM #{@table} WHERE #{column} ~ #{value}")
-=======
 module BrokenCrystals
     class ReadRepository
         @db: DBWrapper
@@ -47,7 +24,6 @@ module BrokenCrystals
             value = value.is_a?(String) ? "'#{value}'" : value
             @db.exec("SELECT * FROM #{@table} WHERE #{column} ~ #{value}")
         end
->>>>>>> feat(user): add user auth
     end
   end
 end
