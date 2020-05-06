4f908a957a8cefc77fd857e533cef0cedb96b79c
mirsad <miki.halilcevic@gmail.com>
2020-05-06 17:31:13 +0200

    format the unformated

--- src/repositories/db_wrapper.cr
+++ src/repositories/db_wrapper.cr
@@ -1,19 +1,19 @@
 require "pg"
 
 module BrokenCrystals
-   class DBWrapper
-        @query_runner: DB::Database
+  class DBWrapper
+    @query_runner : DB::Database
 
-        def initialize
-            @query_runner = DB.open(ENV["POSTGRES_URL"]? || "postgresql://postgres:password@localhost:5432/broken_crystal")
-        end
+    def initialize
+      @query_runner = DB.open(ENV["POSTGRES_URL"]? || "postgresql://postgres:password@localhost:5432/broken_crystal")
+    end
 
-        def exec(query : String)
-            @query_runner.query(query)
-        end
+    def exec(query : String)
+      @query_runner.query(query)
+    end
 
-        def sanitize(values : Array)
-            values.map { |v| v.is_a?(String) ? "'#{v}'" : v }
-        end
-    end 
+    def sanitize(values : Array)
+      values.map { |v| v.is_a?(String) ? "'#{v}'" : v }
+    end
+  end
 end
--- src/repositories/read_repository.cr
+++ src/repositories/read_repository.cr
@@ -1,28 +1,28 @@
 module BrokenCrystals
-    class ReadRepository
-        @db: DBWrapper
+  class ReadRepository
+    @db : DBWrapper
 
-        def initialize(@table : String)
-            @db = DBWrapper.new
-        end
-
-        def select()
-            @db.exec("SELECT * FROM #{@table}")
-        end
-
-        def select_where(column : String, value : String | Int32)
-            value = value.is_a?(String) ? "'#{value}'" : value
-            @db.exec("SELECT * FROM #{@table} WHERE #{column} = #{value}")
-        end
-
-        def select_where_in(column : String, value : Array(String | Int32))
-            value = value.map{ |v| v.is_a?(String) ? "'#{v}'" : v }
-            @db.exec("SELECT * FROM #{@table} WHERE #{column} IN #{value}")
-        end
-
-        def select_where_like(column : String, value : String)
-            value = value.is_a?(String) ? "'#{value}'" : value
-            @db.exec("SELECT * FROM #{@table} WHERE #{column} ~ #{value}")
-        end
+    def initialize(@table : String)
+      @db = DBWrapper.new
     end
+
+    def select
+      @db.exec("SELECT * FROM #{@table}")
+    end
+
+    def select_where(column : String, value : String | Int32)
+      value = value.is_a?(String) ? "'#{value}'" : value
+      @db.exec("SELECT * FROM #{@table} WHERE #{column} = #{value}")
+    end
+
+    def select_where_in(column : String, value : Array(String | Int32))
+      value = value.map { |v| v.is_a?(String) ? "'#{v}'" : v }
+      @db.exec("SELECT * FROM #{@table} WHERE #{column} IN #{value}")
+    end
+
+    def select_where_like(column : String, value : String)
+      value = value.is_a?(String) ? "'#{value}'" : value
+      @db.exec("SELECT * FROM #{@table} WHERE #{column} ~ #{value}")
+    end
+  end
 end
