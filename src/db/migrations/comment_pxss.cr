767e497055d886e096e22279e7c126a509e9d671
mirsad <miki.halilcevic@gmail.com>
2020-05-07 13:08:36 +0200

    refactor(db): strings indent

--- src/db/migrations/comment_pxss.cr
+++ src/db/migrations/comment_pxss.cr
@@ -3,11 +3,12 @@ require "../migration"
 module BrokenCrystals
   class Comments < Migration
     def up
-      @query_runner.query %|CREATE TABLE IF NOT EXISTS
-      "comments" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+      @query_runner.query %|CREATE TABLE IF NOT EXISTS "comments" 
+        ("id" uuid NOT NULL DEFAULT uuid_generate_v4(),
         "content" character varying NOT NULL,
         "name" character varying NOT NULL,
-        CONSTRAINT "PK_comments" PRIMARY KEY ("id"))|
+        CONSTRAINT "PK_comments" PRIMARY KEY ("id")
+        )|
     end
 
     def down
--- src/db/migrations/db_config.cr
+++ src/db/migrations/db_config.cr
@@ -3,13 +3,13 @@ require "../migration"
 module BrokenCrystals
   class Initial < Migration
     def up
-      @query_runner.query("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
-      @query_runner.query("CREATE EXTENSION IF NOT EXISTS \"btree_gist\";")
+      @query_runner.query(%|CREATE EXTENSION IF NOT EXISTS "uuid-ossp";|)
+      @query_runner.query(%|REATE EXTENSION IF NOT EXISTS "btree_gist";|)
     end
 
     def down
-      @query_runner.query("DROP EXTENSION IF EXISTS \"uuid-ossp\" CASCADE;")
-      @query_runner.query("DROP EXTENSION IF EXISTS \"btree_gist\" CASCADE;")
+      @query_runner.query(%|DROP EXTENSION IF EXISTS "uuid-ossp" CASCADE;|)
+      @query_runner.query(%|DROP EXTENSION IF EXISTS "btree_gist" CASCADE;|)
     end
   end
 end
