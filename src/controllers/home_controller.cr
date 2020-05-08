ebad2b900ee12e6f32a98bb7f5c050dd5f1608a8
Faris Seferagic <farisseferagic@gmail.com>
2020-05-08 17:14:42 +0200

    fix(formatting): There you go Mirsad :P

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -3,15 +3,14 @@ require "../repositories/*"
 module BrokenCrystals
   module HomeController
     HOME_PREFIX = ""
-      get "/" do |env|
-        env.response.headers["Content-Type"] = "text/html"
-        render "src/views/main.ecr"
-      end
+    get "/" do |env|
+      env.response.headers["Content-Type"] = "text/html"
+      render "src/views/main.ecr"
+    end
 
-      get "/vulns" do |env|
-        env.response.headers["Content-Type"] = "text/html"
-        render "src/views/vulns.ecr"
-      end
-
+    get "/vulns" do |env|
+      env.response.headers["Content-Type"] = "text/html"
+      render "src/views/vulns.ecr"
+    end
   end
 end
