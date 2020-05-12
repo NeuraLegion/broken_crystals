25f30a4f6c10fa7448141a75a034702a8a7df887
Faris Seferagic <farisseferagic@gmail.com>
2020-05-12 15:52:01 +0200

    feat(marketplace): Added some test frontend

---
+++ src/controllers/shop_controller.cr
@@ -0,0 +1,25 @@
+require "../repositories/*"
+require "../models/*"
+require "kemal"
+require "kemal-session"
+
+module BrokenCrystals
+  module UserController
+    SHOP_PREFIX = "shop"
+    mutable_repo = MutableRepository.new "shop"
+    read_repo = ReadRepository.new "shop"
+
+    get "/marketplace" do |env|
+      title = "Marketplace"
+      env.response.headers["Content-Type"] = "text/html"
+      render "src/views/shop/listing.ecr", "src/views/shop/layout.ecr"
+    end
+
+    get "/item" do |env|
+      title = "Item"
+      env.response.headers["Content-Type"] = "text/html"
+      render "src/views/shop/detail.ecr", "src/views/shop/layout.ecr"
+    end
+
+  end
+end
